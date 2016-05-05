6e7240221dd043fec16c8e2f8ee8f2183151cc6c
Mr. Is <isaac.ardis@gmail.com>
2016-05-04 20:01:21 -0400

    clear cache

--- lib/server/aliases.sh
+++ lib/server/aliases.sh
@@ -1,5 +1,5 @@
 # To include with the server's cmd aliases. 
-alias apache_reload="service apache 2 reload"
-alias ptat="passenger-status"
+alias apre="service apache 2 reload"
+alias pstat="passenger-status"
 alias pms="passenger-memory-stats"
 alias preq="passenger-status -show=requests"
