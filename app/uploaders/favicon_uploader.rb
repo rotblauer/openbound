89c46ed8afaf8c2c0b1461f58ed70961f8057c85
Mr. Is <isaac.ardis@gmail.com>
2017-03-04 11:26:02 +0100

    Remove mime types from favicon uploader.

--- app/uploaders/favicon_uploader.rb
+++ app/uploaders/favicon_uploader.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-require 'carrierwave/processing/mime_types'
+# require 'carrierwave/processing/mime_types'
 require 'carrierwave/processing/rmagick'
 class FaviconUploader < CarrierWave::Uploader::Base
 
