8481122367b5d8aac1af96d181b944fb595fdfde
Mr. Is <isaac.ardis@gmail.com>
2017-03-09 12:58:22 +0100

    Use different port for ssh on prod.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,11 +41,12 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(~/ia/.ssh/id_rsa_mh),
+   forward_agent: false,
+   # auth_methods: %w(password),
+   port: 1026
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
