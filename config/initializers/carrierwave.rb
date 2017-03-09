e87e2f2421d20d363d4064d4f6ce4bd8d3ce1c72
Mr. Is <isaac.ardis@gmail.com>
2017-03-09 15:48:42 +0100

    :floppy_disk:

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,13 +6,13 @@ require 'carrierwave/storage/fog'
 require 'carrierwave/processing/rmagick'
 if Rails.env.production?
   CarrierWave.configure do |config|
-    # This was turned off and worked fine. 
+    # This was turned off and worked fine.
     # config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID_I'],                        # required # Isaac's
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_I']                        # required
-      # region:                'us-west-2'                  # optional, defaults to 'us-east-1'
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_I'],                        # required
+      region:                'us-west-2'                  # optional, defaults to 'us-east-1'
     }
     config.storage :fog
     config.fog_directory  = 'openbound-library'                          # required
@@ -21,17 +21,17 @@ if Rails.env.production?
   end
 end
 
-if Rails.env.development? 
+if Rails.env.development?
 	CarrierWave.configure do |config|
     config.storage :file
-    # Turns noisy errors on for development. Don't do much. 
+    # Turns noisy errors on for development. Don't do much.
 		config.ignore_integrity_errors = false
 		config.ignore_processing_errors = false
 		config.ignore_download_errors = false
 	end
 end
 
-# Turns :file storage on and processing off for testes. 
+# Turns :file storage on and processing off for testes.
 if Rails.env.test?
 	CarrierWave.configure do |config|
 		config.storage = :file
