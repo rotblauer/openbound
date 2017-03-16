55ed831f3582d95f08b81b709e00d1aab208981c
Mr. Is <isaac.ardis@gmail.com>
2017-03-16 16:15:20 +0100

    keep fewer assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :keep_assets, 2
 
 ## Defaults:
 set :scm,             :git #:gitcopy
