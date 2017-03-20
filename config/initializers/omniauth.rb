b78f4e24055aa5162dbf0062f581c78893507b93
Mr. Is <isaac.ardis@gmail.com>
2017-03-20 18:10:18 +0100

    use env instead of railssecs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # https://github.com/mkdynamic/omniauth-facebook
   provider :developer if Rails.env.development?
-  provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret,
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
   	:scope => 'email,public_profile,user_education_history', :info_fields => 'id,name,email,education', :display => 'page' # should return default info
 end
