194b7f0835ae5db362f888c98ba7d24ef742018c
Mr. Is <isaac.ardis@gmail.com>
2017-06-03 09:20:08 -0500

    I really should test locally before pushing upstream

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -48,6 +48,8 @@ class ProjectsController < ApplicationController
     desired_number_of_school_facets = 15
     @facet_schools = School.order('works_count desc').order('affiliations_count desc').order('updated_at desc').first(desired_number_of_school_facets)
 
+    @works_count = Work.all.count
+
 
     # if @projects.any?
     #   # Schools associated with resulting works (first page),
